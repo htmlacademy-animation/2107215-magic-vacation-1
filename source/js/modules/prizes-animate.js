64dac8f8ec0e0cf7fad0532e3d7e77416dc2fcce
Alena59 <asdalena59@gmail.com>
2023-10-06 04:32:00 +0300

    airship animation

---
+++ source/js/modules/prizes-animate.js
@@ -0,0 +1,25 @@
+export default class PrizesAnimation {
+  // constructor() {
+  //   this.footerTogglers = document.querySelector(`.prizes__primary-award-from animate`);
+  // }
+
+  // init() {
+  //   this.showFooterFull();
+  // }
+
+  // showFooterFull() {
+  //   if (this.footerTogglers.length) {
+  //     for (let i = 0; i < this.footerTogglers.length; i++) {
+  //       this.footerTogglers[i].addEventListener(`click`, () => {
+  //         let footer = this.footerTogglers[i].parentNode;
+  //         if (footer.classList.contains(`screen__footer--full`)) {
+  //           footer.classList.remove(`screen__footer--full`);
+  //         } else {
+  //           footer.classList.add(`screen__footer--full`);
+
+  //         }
+  //       });
+  //     }
+  //   }
+  // }
+}
